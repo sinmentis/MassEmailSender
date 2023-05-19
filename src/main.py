38cd749a90c8b880b38ed44d7d54d98413ab0798
sinmentis <sinmentis@foxmail.com>
2023-05-19 17:00:38 +1200

    Adjust file structure

--- src/main.py
+++ src/main.py
@@ -3,11 +3,10 @@
 
 from MassEmailSender import MassEmailSender as MES
 
-
 def main():
-    sender_list = MES.parse_sender_data("../config_json/sender.json")
-    email = MES.parse_email_to_send("../config_json/email_to_send.json")
-    destination_list = MES.parse_destination_list("../config_json/destination_list.json")
+    sender_list = MES.parse_sender_data("./config_json/sender.json")
+    email = MES.parse_email_to_send("./config_json/email_to_send.json")
+    destination_list = MES.parse_destination_list("./config_json/destination_list.json")
     email_sender = MES.email_worker(destination_list, sender_list, email, debug_only=True)
     email_sender.start_sending()
 
